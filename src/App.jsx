9d50e281ed1154a70b9a21ebc36135005f415f5b
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2024-05-14 17:57:52 -0400

    funcion de calorías
    -Se implemento la funcion para calcular calorías.

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ function App() {
         { value: "1.9", label: "Muy Activo" },
     ];
 
+    const handleButtonClick = () => {
+        calorieCalculator();
+    };
+
     return (
         <>
           <Navbar/>
@@ -28,11 +32,12 @@ function App() {
                     <div className="card-body">
                         <h5 className="card-title">Calculadora</h5>
                         <h6 className="card-subtitle mb-2 text-muted">El resultado de esta calculadora no es exacto y deberia ser utilizado com un aproximado.</h6>
-                        <InputSelect options={selectGender} selectText="Sexo" selectPlaceholder="Seleccione su sexo" />
+                        <InputSelect options={selectGender} selectText="Sexo" selectPlaceholder="Seleccione su sexo" selectId="selectGender" />
                         <Input inputText="Edad" inputId="inputAge" inputType="number" inputPlaceholder=""/>
-                        <Input inputText="Altura" inputId="inputAge" inputType="number" inputPlaceholder=""/>
-                        <Input inputText="Peso" inputId="inputAge" inputType="number" inputPlaceholder="" />
-                        <InputSelect options={selectActivity} selectText="Actividad" selectPlaceholder="Seleccione su nivel de actividad" />
+                        <Input inputText="Altura" inputId="inputHeight" inputType="number" inputPlaceholder=""/>
+                        <Input inputText="Peso" inputId="inputWeight" inputType="number" inputPlaceholder="" />
+                        <InputSelect options={selectActivity} selectText="Actividad" selectPlaceholder="Seleccione su nivel de actividad" selectId="selectActivity"/>
+                        <button type="button" className="btn btn-primary" onClick={handleButtonClick}>Primary</button>
                     </div>
                 </div>
             </div>
@@ -41,3 +46,21 @@ function App() {
 }
 
 export default App
+
+function calorieCalculator() {
+    let age = document.getElementById('inputAge').value;
+    let height = document.getElementById('inputHeight').value;
+    let weight = document.getElementById('inputWeight').value;
+    let gender = document.getElementById('selectGender').value;
+    let activity = document.getElementById('selectActivity').value;
+
+    let calories;
+
+    if (gender == 1) {
+        calories = 5 + (10 * weight) + (6.25 * height) - (5 * age);
+    } else {
+        calories = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+    }
+    calories *= activity;
+    console.log(calories);
+}
--- src/components/InputSelect.jsx
+++ src/components/InputSelect.jsx
@@ -1,10 +1,10 @@
-const InputSelect = ({ selectText, options, selectPlaceholder }) => {
+const InputSelect = ({ selectText, options, selectPlaceholder, selectId }) => {
     return (
         <div className="input-group mb-3">
             <div className="input-group-prepend">
                 <label className="input-group-text" htmlFor="inputGroupSelect01">{selectText}</label>
             </div>
-            <select className="form-select" id="inputGroupSelect01">
+            <select className="form-select" id={selectId}>
                 <option >{selectPlaceholder}</option>
                 {options.map((option, index) => (
                     <option key={index} value={option.value}>{option.label}</option>
