e3e7ad66fff2f194b0fe1774772878e9f0a9a19b
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2024-05-20 18:42:29 -0400

    Actualización estetica de la calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /*import { useState } from 'react'*/
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-//import './App.css'
+import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Input from './components/Input.jsx'
 import InputSelect from './components/InputSelect.jsx'
@@ -26,21 +26,39 @@ function App() {
 
     return (
         <>
-          <Navbar/>
-            <div>
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Calculadora</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">El resultado de esta calculadora no es exacto y deberia ser utilizado com un aproximado.</h6>
-                        <InputSelect options={selectGender} selectText="Sexo" selectPlaceholder="Seleccione su sexo" selectId="selectGender" />
-                        <Input inputText="Edad" inputId="inputAge" inputType="number" inputPlaceholder=""/>
-                        <Input inputText="Altura" inputId="inputHeight" inputType="number" inputPlaceholder=""/>
-                        <Input inputText="Peso" inputId="inputWeight" inputType="number" inputPlaceholder="" />
-                        <InputSelect options={selectActivity} selectText="Actividad" selectPlaceholder="Seleccione su nivel de actividad" selectId="selectActivity"/>
-                        <button type="button" className="btn btn-primary" onClick={handleButtonClick}>Primary</button>
+            <Navbar />
+            <div className="content">
+                <div className="container row py-4">
+                    <div className="calculator col-8 mx-auto px-0">
+                        <div className="calculator-title p-2 text-white">
+                            <h5 className="card-title text-center">Calculadora</h5>
+                            <h6 className="card-subtitle mb-2 ">El resultado de esta calculadora no es exacto y deberia ser utilizado como un aproximado.</h6>
+                        </div> 
+                        <div className="calculator-body p-2 px-3">                                                   
+                            <InputSelect options={selectGender} selectText="Sexo" selectPlaceholder="Seleccione su sexo" selectId="selectGender" />
+                            <Input inputText="Edad" inputId="inputAge" inputType="number" inputPlaceholder="" />
+                            <Input inputText="Altura" inputId="inputHeight" inputType="number" inputPlaceholder="" />
+                            <Input inputText="Peso" inputId="inputWeight" inputType="number" inputPlaceholder="" />
+                            <InputSelect options={selectActivity} selectText="Actividad" selectPlaceholder="Seleccione su nivel de actividad" selectId="selectActivity" />
+                            <button type="button" className="btn btn-primary" onClick={handleButtonClick}>Calcular</button>
+                        </div>
+                    </div>
+                    <div id="results" className="results col-4 d-none">
+                        <div className="main-calories mb-3 text-center p-2">
+                            <h6 className="text-white ">Calor&iacute;as diarias</h6>
+                            <h1 className="text-white" id="mainCalories"></h1>
+                        </div>
+                        <div className="secondary-calories mb-3 text-center p-2">
+                            <h6 className=" ">Calor&iacute;as para perder peso</h6>
+                            <h1 className="" id="cutCalories"></h1>
+                        </div>
+                        <div className="secondary-calories mb-3 text-center p-2">
+                            <h6 className=" ">Calor&iacute;as para ganar peso</h6>
+                            <h1 className="" id="bulkCalories"></h1>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div>          
         </>
     )
 }
@@ -48,13 +66,16 @@ function App() {
 export default App
 
 function calorieCalculator() {
+    let results = document.getElementById('results');
+    results.classList.remove('d-none');
+
     let age = document.getElementById('inputAge').value;
     let height = document.getElementById('inputHeight').value;
     let weight = document.getElementById('inputWeight').value;
     let gender = document.getElementById('selectGender').value;
     let activity = document.getElementById('selectActivity').value;
 
-    let calories;
+    let calories, cutCal, bulkCal;
 
     if (gender == 1) {
         calories = 5 + (10 * weight) + (6.25 * height) - (5 * age);
@@ -62,5 +83,13 @@ function calorieCalculator() {
         calories = (10 * weight) + (6.25 * height) - (5 * age) - 161;
     }
     calories *= activity;
-    console.log(calories);
+    cutCal = calories - 350;
+    bulkCal = calories * 1.15;
+
+    let mainCalories = document.getElementById('mainCalories');
+    let cutCalories = document.getElementById('cutCalories');
+    let bulkCalories = document.getElementById('bulkCalories');
+    mainCalories.innerText = Math.trunc(calories);
+    cutCalories.innerText = Math.trunc(cutCal);
+    bulkCalories.innerText = Math.trunc(bulkCal);
 }
