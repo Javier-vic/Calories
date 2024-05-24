f5f49127a2eac1566fb2917743497b3f2c45d006
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2024-05-24 19:10:52 -0400

    Sección de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Calculator from './components/Calculator.jsx'
 import Formula from './components/Formula.jsx'
+import Project from './components/Project.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                     <Routes >
                         <Route exact path="/" element={<Calculator/>} />
                         <Route path="/funcionamiento" element={<Formula />} />
+                        <Route path="/proyecto" element={<Project />} />
                     </Routes>                              
                 </div>
                 <Footer/>
---
+++ src/components/Project.jsx
@@ -0,0 +1,29 @@
+function Project() {
+    return (
+        <div className="container row py-4">
+            <div className="formula col-8 mx-auto p-3">
+                <h3 className="text-color-1 text-center">Sobre la Calculadora de Calor&iacute;as</h3>
+                <p>
+                    Esta SPA de una calculadora de calor&iacute;as fue desarrollada con el objetivo de practicar React.
+                    Es una herramienta sencilla que permite a los usuarios calcular su ingesta cal&oacute;rica diaria
+                    utilizando la f&oacute;rmula de Mifflin-St Jeor. Este proyecto es de c&oacute;digo abierto, y cualquier persona
+                    puede utilizar el c&oacute;digo disponible en GitHub.
+                </p>
+                <p>
+                    Puedes encontrar el c&oacute;digo fuente en el siguiente enlace:
+                    <a href="https://github.com/Javier-vic/Calories" target="_blank" rel="noopener noreferrer">
+                        Repositorio de GitHub
+                    </a>.
+                </p>
+                <p>
+                    Para ver m&aacute;s de mis proyectos y mi portafolio, visita mi sitio web:
+                    <a href="https://javiervictoriano.com" target="_blank" rel="noopener noreferrer">
+                        Mi Portafolio
+                    </a>.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
